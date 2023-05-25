d8d87d08960103017accd9f8292a6910ec75dd1d
Ivo Dimitrov <idimitrov@easypay.bg>
2023-05-25 21:53:51 +0300

    add policy attach fix lambda fix gateway method response

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region  = "eu-central-1"
+    region  = "eu-west-3"
     profile = "default"
 }
 
@@ -17,6 +17,7 @@ data "aws_iam_policy_document" "dynamoDbCloudWatchLogs" {
 }
 
 resource "aws_iam_policy" "dynamoDbCloudWatchLogs" {
+   name         = "dynamoDbCloudWatchLogs"
    description  = "Custom policy with permission to DynamoDB and CloudWatch Logs. This custom policy has the permissions that the function needs to write data to DynamoDB and upload logs"
    policy       = "${file("policy.json")}"
 }
@@ -27,16 +28,21 @@ resource "aws_iam_role" "lambda-apigateway-role" {
   assume_role_policy = data.aws_iam_policy_document.dynamoDbCloudWatchLogs.json
 }
 
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.lambda-apigateway-role.name
+  policy_arn = aws_iam_policy.dynamoDbCloudWatchLogs.arn
+}
+
 data "archive_file" "python_lambda_package" {
   type        = "zip"
-  source_file = "lambda.py"
+  source_file = "lambda_function.py"
   output_path = "lambda_function_payload.zip"
 }
 
 resource "aws_lambda_function" "LambdaFunctionOverHttps" {
    
     function_name                  = "LambdaFunctionOverHttps"
-    filename                       = "lambda.zip"
+    filename                       = "lambda_function_payload.zip"
     source_code_hash               = data.archive_file.python_lambda_package.output_base64sha256
     handler                        = "lambda_function.lambda_handler"
     role                           = aws_iam_role.lambda-apigateway-role.arn
@@ -44,6 +50,7 @@ resource "aws_lambda_function" "LambdaFunctionOverHttps" {
     timeout                        = 3
 
 }
+
 
 resource "aws_dynamodb_table" "lambda-apigateway" {
     
@@ -89,15 +96,40 @@ resource "aws_api_gateway_integration" "lambda" {
     integration_http_method = "POST"
     resource_id             = aws_api_gateway_resource.dynamodbmanager.id
     rest_api_id             = aws_api_gateway_rest_api.DynamoDBOperations.id
-    timeout_milliseconds    = 29000
     type                    = "AWS"
     uri                     = aws_lambda_function.LambdaFunctionOverHttps.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.DynamoDBOperations.id
+  resource_id = aws_api_gateway_resource.dynamodbmanager.id
+  http_method = aws_api_gateway_method.POST.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+    }
+}
+
+resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.DynamoDBOperations.id
+  resource_id = aws_api_gateway_resource.dynamodbmanager.id
+  http_method = aws_api_gateway_method.POST.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+
 }
 
 resource "aws_api_gateway_deployment" "Prod" {
     depends_on      = [ aws_api_gateway_integration.lambda ]
     rest_api_id     = aws_api_gateway_rest_api.DynamoDBOperations.id
     stage_name      = "Prod"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.LambdaFunctionOverHttps.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.DynamoDBOperations.execution_arn}/*/POST/dynamodbmanager"
 }
 
 output "aws_api_gateway_deployment" {
